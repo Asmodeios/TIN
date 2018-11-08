42a5ab9e88a247459ae78e235cec4a38a9eb0726
Asmodeios <33784452+Asmodeios@users.noreply.github.com>
2018-11-08 13:38:30 +0100

    fixed 5d

--- Lab5/5d.js
+++ Lab5/5d.js
@@ -7,10 +7,13 @@ function Student(first, last, id, grades) {
   Object.defineProperties(this, {
     'avgGrade' : { get: function() { return this.grades.reduce((a,b) => a + b, 0) / this.grades.length - 1; }},
     'getFullName' : { get: function() { return this.firstName + ' ' + this.lastName; }},
-    'setFullName' : { set: function(first, last) { this.firstName = first; this.lastName = last; }}
+    'setFullName' : { set: function(fullName) { this.firstName = fullName[0]; this.lastName = fullName[1]; }}
   });
 };
 
 var student = new Student("Dima", "Lentay", 1, [5,5,2,5]);
 
 console.log(student.avgGrade);
+console.log(student.getFullName);
+student.setFullName = ["Igor", "Programmer"];
+console.log(student.getFullName);
