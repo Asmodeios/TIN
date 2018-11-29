c6c67cbe707fccb5ab08a504c3cdb655ad722714
Asmodeios <33784452+Asmodeios@users.noreply.github.com>
2018-11-29 12:39:19 +0100

    Fixed

--- Lab7/7b/7b.js
+++ Lab7/7b/7b.js
@@ -2,12 +2,9 @@ const fs = require('fs');
 require('log-timestamp');
 let args = process.argv.slice(2);
 
-const testFolder = args[0];
+const testFolder = args[0] + '/';
 
-/*
-*  Logs to console twice for some reason
-*/
-fs.watch(testFolder , (eventType, filename) => {
+fs.watch(testFolder, (eventType, filename) => {
   console.log(`${eventType}d ${filename}`);
   fs.readFile(testFolder + filename, 'utf-8', (err, data) => {
     if (err) {
